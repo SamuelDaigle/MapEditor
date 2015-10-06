3abcfb185b08790cd23b89a9420d37dce77e25d0
Vincent Montminy <vingt_cent_20@hotmail.com>
2015-10-06 14:57:41 -0400

    Squelette Validator

---
+++ Map Editor/Map Editor/Model/Validator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Map_Editor.GameData
+{
+    class Validator
+    {
+        private Scene sceneToValidate;
+
+        public Validator(Scene _scene)
+        {
+            sceneToValidate = _scene;
+        }
+
+        public bool ValidateMap()
+        {
+            
+        }
+
+        private bool validateFirstFloorEmpty()
+        {
+            
+        }
+
+        private bool validateTeleporters()
+        {
+            
+        }
+
+        private bool validateSlopes()
+        {
+            
+        }
+
+        private bool validateTowers()
+        {
+            
+        }
+    }
+}
